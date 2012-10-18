0510f38702aaffc03c3c81723f7400aab4979b88
Anh Nguyen <ahnng@seas.upenn.edu>
2012-10-18 00:19:40 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ongaku::Application.config.secret_token = 'e869f3960bf3017c49f9d8d5b7ef4add4cbe8f4d8a377b4731551b70aedf7279438340f03bed3d6782f69efef68d0d113fa32cb73d85297f740d93cb83a3df1d'
