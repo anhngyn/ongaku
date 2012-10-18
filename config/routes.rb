bfe96a02c2dcc20a8680b625475352b79a5c6c25
Anh Nguyen <ahnng@seas.upenn.edu>
2012-10-18 01:22:39 -0400

    finish demo app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Ongaku::Application.routes.draw do
+
+
+  resources :microposts
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +54,7 @@ Ongaku::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
@@ -56,3 +62,5 @@ Ongaku::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 end
+
+
