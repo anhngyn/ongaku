2c038c4e34a90266f3894eb85d5a1b212bfa9ca1
Anh Nguyen <ahnng@seas.upenn.edu>
2012-11-16 16:14:24 -0500

    added jquery toggle button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,15 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
+$(function() {
+$('#toggleButton').click( function() {
+  $('#header').toggle('5000');
+  $('#footer').toggle('5000', function(){
+ if ($('#header').is(':visible')) {
+  $('#toggleButton').val('Hide') 
+ } else {
+  $('#toggleButton').val('Show') 
+   }
+    });
+  });
+});
